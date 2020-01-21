898bc1f70b8f715f5e770bf7a46e3d98c493b941
Alex Medina <info@alexmedina.net>
2020-01-21 20:44:05 +0100

    mongoose

--- 05-Restserver/server/server.js
+++ 05-Restserver/server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,36 +12,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
+
+async function conectarBD(){
+    await mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }).catch(error => console.log(error));
+
+    console.log('conexion establecida con BD');
+}
+
+conectarBD();
+
+
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-})
- 
-app.post('/usuario', function (req, res) {
 
-    let body = req.body;
-
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        });
-    }
-    else {
-        res.json({
-            persona: body
-        })
-    }
-})
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({id})
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-})
 
 app.listen(process.env.PORT , () => {
     console.log(`Escuchando puerto ${process.env.PORT}`);
