5b5b3bf3380e9cc02c1c529228ce0f8876c96933
Alex Medina <info@alexmedina.net>
2020-02-27 19:04:47 +0100

    config

--- 05-Restserver/server/config/config.js
+++ 05-Restserver/server/config/config.js
@@ -3,11 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// }
-// else {
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}
+else {
     urlDB = 'mongodb+srv://amedinadevs:IgrSnyOR0efNh0qB@cluster0-i7pp7.mongodb.net/cafe';
-//}
+}
 
 process.env.URLDB = urlDB;
