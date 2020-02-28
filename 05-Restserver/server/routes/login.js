033c04d4ea9686867b9ed26cb5183327ba7afd48
Alex Medina <info@alexmedina.net>
2020-02-28 20:08:00 +0100

    jwt 1

---
+++ 05-Restserver/server/routes/login.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Usuario = require('../models/usuario');
+const app = express();
+//require('../config/config');
+
+app.post('/login',(req,res)=>{
+
+    let body = req.body;
+    Usuario.findOne({email: body.email}, (err, usuarioBD)=>{
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if(!usuarioBD){
+            return res.status(400).json({
+                ok: false,
+                err: {message: '->Usuario o contraseña incorrecto'}
+            });
+        }
+        if(!bcrypt.compareSync(body.password, usuarioBD.password)){
+            return res.status(400).json({
+                ok: false,
+                err: {message: 'Usuario o ->contraseña incorrecto'}
+            });
+        }
+
+        var token = jwt.sign(
+            {usuario: usuarioBD}, 
+            process.env.TOKEN_SEED,
+            {expiresIn:process.env.TOKEN_CADUCIDAD}
+        );
+
+        res.json({
+            ok: true,
+            usuario: usuarioBD,
+            token: token
+        })
+    })
+
+})
+
+
+
+module.exports = app;
